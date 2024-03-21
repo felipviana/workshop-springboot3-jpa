93b013c32651233cbdf951c4c543dc23b8f4acc8
Felps <fabiofelipevianasantosfacul@gmail.com>
2024-03-21 17:08:12 -0300

    Product

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
@@ -28,14 +30,25 @@ public class TestConfig implements CommandLineRunner{
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, conectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Lorem ipsum dolor sit amet, conectetur.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Lorem ipsum dolor sit amet, conectetur.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Lorem ipsum dolor sit amet, conectetur.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Lorem ipsum dolor sit amet, conectetur.", 100.99, "");
 
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com","988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com","977777777", "123456");
